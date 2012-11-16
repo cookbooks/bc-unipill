d51df2ce8cf491315dd1f460c544576cb6852631
Daniel Hahn <dha@betterplace.org>
2012-11-16 14:13:13 +0100

    Minor Fixes

--- recipes/default.rb
+++ recipes/default.rb
@@ -88,8 +88,8 @@ end
               :rails_env => node.unipill.rails_env,
               :log_file => "#{node.unipill.shared}/bluepill/log/#{pill_name}.log",
               :pid_file => "#{node.unipill.shared}/pids/#{pill_name}.pid",
-              :bin_path => "#{node.unipill.bin_root}/bin"),
-              :garbage_colect_settings => garbage_collect_file
+              :bin_path => "#{node.unipill.bin_root}/bin",
+              :garbage_colect_settings => garbage_collect_file)
   end
 end
 
@@ -127,7 +127,7 @@ template "#{node.unipill.bin_root}/bin/bpill" do
 end
 
 template "#{node.unipill.bin_root}/bin/process_killer" do
-  source 'process_killer.erb'
+  source 'scripts/process_killer.erb'
   owner 'root'
   group 'root'
   mode '0755'
