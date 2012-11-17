f18842341735762e7855fc5e45163d56a64e848a
Daniel Hahn <dha@betterplace.org>
2012-11-17 14:36:48 +0100

    Tweak memory settings and bluepill conditions

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,7 @@ default.unipill.bluepill_root = '/opt/local/bluepill'
 default.unipill.bin_paths = ["/opt/local/bin", '/opt/chef/embedded/bin']
 default.unipill.gem_paths = ["/opt/chef/embedded/lib/ruby/gems/1.9.1", "/opt/chef/embedded/lib/ruby/gems/1.9.1/@global"]
 default.unipill.ruby_version = '1.9.3'
-default.unipill.garbage_collect_settings = "RUBY_HEAP_MIN_SLOTS=4000000 RUBY_HEAP_SLOTS_INCREMENT=600000 RUBY_HEAP_SLOTS_GROWTH_FACTOR=1.4 RUBY_GC_MALLOC_LIMIT=1000000000 RUBY_HEAP_FREE_MIN=50000 "
+default.unipill.garbage_collect_settings = "RUBY_HEAP_MIN_SLOTS=1300000 RUBY_HEAP_SLOTS_INCREMENT=80000 RUBY_HEAP_SLOTS_GROWTH_FACTOR=1.2 RUBY_GC_MALLOC_LIMIT=80000000 RUBY_HEAP_FREE_MIN=4096"
 
 # Unicorn settings
 default.unipill.worker_timeout = 180
